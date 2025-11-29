5a983da061e10cb99ce1974f0a385d4eef459a17
theGuitar1234 <turanlizad776@gmail.com>
2025-11-29 16:16:03 +0400

    Update 6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-print("Welcome to " + str1 + " " + str2 + "!", end="")
+print("Welcome to " + str1 + " " + str2 + "!" + "\n", end="")
 print("", end="")
