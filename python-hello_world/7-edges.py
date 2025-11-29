2e7bffd702e71b0b628658f4cf430bdcf5887f82
theGuitar1234 <turanlizad776@gmail.com>
2025-11-29 16:19:57 +0400

    Update and rename file1 to 7-edges.py

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+print(f"First 3 letters: {word[0:3]}")
+print(f"Last 2 letters: {word[-2:len(word)]}")
+print(f"Middle word: {word[1:len(word)-1]}")
+print("", end="")
+print("", end="")
+print("", end="")
