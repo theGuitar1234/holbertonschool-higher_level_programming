002d1ff691ffd243956e314eaf009cb4f915f6c6
theGuitar1234 <turanlizad776@gmail.com>
2025-11-29 16:49:04 +0400

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str.split(" ")[5] + " " + str.split(" ")[6] + " " + str.split(" ")[12] + " " + str.split(" ")[0])
+print(str[39:67] + str[-22:-17] + str[0:6])
 print("", end="")
