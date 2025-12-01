c84a0f26c5ee344a34bd29388ae03212b8788266
d <sdf>
2025-12-01 10:06:27 +0000

    a1

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,8 +4,8 @@
 def replace_in_list(my_list, idx, element):
 
     if idx < 0:
-        return None
+        return my_list
     elif idx > len(my_list)-1:
-        return None
+        return my_list
     my_list[idx] = element
     return my_list
