4d4427a4abb9418edfa35bfe79a6d766af0cff58
dfsd <dfs>
2025-12-01 08:10:52 +0000

    a1

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import calculator_1 as calc
+    import sys
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> operator <b>")
+        sys.exit(1)
+    else:
+        a, b = sys.argv[1], sys.argv[3]
+        match sys.argv[2]:
+            case "+":
+                print("{} + {} = {}".format(a, b, calc.add(a, b)))
+            case "-":
+                print("{} - {} = {}".format(a, b, calc.add(a, b)))
+            case "*":
+                print("{} * {} = {}".format(a, b, calc.add(a, b)))
+            case "/":
+                print("{} / {} = {}".format(a, b, calc.add(a, b)))
+            case _:
+                print("Unknown operator. Available operators: +, -, * and /")
+                sys.exit(1)
+        sys.exit(0)
