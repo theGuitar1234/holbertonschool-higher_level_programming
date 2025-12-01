467df562f3fc2ee0f1964869fc9d951ec03c9a01
dfsd <dfs>
2025-12-01 05:07:33 +0000

    a1

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,9 +2,9 @@
 
 if __name__ == "__main__":
 
-    import add_0
+    import add_0 as ad
 
     a = 1
     b = 2
     
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    print("{} + {} = {}".format(a, b, ad.add(a, b)))
