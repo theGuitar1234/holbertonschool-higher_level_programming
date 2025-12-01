8a3c2d6a79951fea6070ec663361df842d4221ad
dfsd <dfs>
2025-12-01 08:18:07 +0000

    a1

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> operator <b>")
+        print("./100-my_calculator.py <a> operator <b>")
         sys.exit(1)
     else:
         a, b, c = int(sys.argv[1]), int(sys.argv[3]), sys.argv[2]
