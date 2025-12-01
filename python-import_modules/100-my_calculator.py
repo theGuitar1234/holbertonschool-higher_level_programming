cf5da32e3172a685694726326072273f77eaf5b8
dfsd <dfs>
2025-12-01 08:15:53 +0000

    a1

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -8,18 +8,17 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> operator <b>")
         sys.exit(1)
     else:
-        a, b = int(sys.argv[1]), int(sys.argv[3])
-        match sys.argv[2]:
-            case "+":
-                print("{} + {} = {}".format(a, b, calc.add(a, b)))
-            case "-":
-                print("{} - {} = {}".format(a, b, calc.add(a, b)))
-            case "*":
-                print("{} * {} = {}".format(a, b, calc.add(a, b)))
-            case "/":
-                print("{} / {} = {}".format(a, b, calc.add(a, b)))
-            case _:
-                print("Unknown operator. Available operators: +, -, * and /")
-                sys.exit(1)
+        a, b, c = int(sys.argv[1]), int(sys.argv[3]), sys.argv[2]
+        if c == "+":
+            print("{} + {} = {}".format(a, b, calc.add(a, b)))
+        elif c == "-":
+            print("{} - {} = {}".format(a, b, calc.add(a, b)))
+        elif c == "*":
+            print("{} * {} = {}".format(a, b, calc.add(a, b)))
+        elif c == "/":
+            print("{} / {} = {}".format(a, b, calc.add(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
         sys.exit(0)
 
