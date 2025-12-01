8b37d1bf2194691d559752db13e71de161c9c910
dfsd <dfs>
2025-12-01 05:48:33 +0000

    a1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         else:
             print("{} arguments:".format(len(sys.argv)-1))
         for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i])
+            print("{}: {}".format(i, sys.argv[i]))
