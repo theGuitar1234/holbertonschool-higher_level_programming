17d69731749551ba2cf6dcc75881a3b5ee7ff77d
d <sdf>
2025-12-01 10:58:01 +0000

    a1

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -7,5 +7,6 @@ def print_matrix_integer(matrix=[[]]):
         for j in range(len(matrix[i])):
             if j == len(matrix[i])-1:
                 print("{:d}".format(matrix[i][j]), end="")
-            print("{:d}".format(matrix[i][j]), end=" ")
+            else:
+                print("{:d}".format(matrix[i][j]), end=" ")
         print()
