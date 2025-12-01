fa97fce676a17efbafbb644232fdeb2a0bcdb075
dfsd <dfs>
2025-12-01 05:22:53 +0000

    a1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, calc.add(a, b)))
-    print("{} + {} = {}".format(a, b, calc.sub(a, b)))
-    print("{} + {} = {}".format(a, b, calc.mul(a, b)))
-    print("{} + {} = {}".format(a, b, calc.div(a, b)))
+    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calc.div(a, b)))
 
