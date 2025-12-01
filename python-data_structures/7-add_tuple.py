514ccd95531440e3b82e9795837ba4fa788162c2
d <sdf>
2025-12-01 11:19:28 +0000

    a1

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,12 +3,24 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
 
+    frst_a = 0
+    frst_b = 0
+    scnd_a = 0
+    scnd_b = 0
+
     if len(tuple_a) == 1:
-        tuple_a + (0,)
+        frst_a = tuple_a[0]
+        scnd_a = 0
     if len(tuple_a) == 0:
-        tuple_a = (0, 0)
-    if (tuple_b) == 1:
-        tuple_b + (0,)
-    if (tuple_b) == 0:
-        tuple_b = (0, 0)
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+        frst_a, scnd_a = 0, 0
+    if len(tuple_b) == 1:
+        frst_b = tuple_b[0]
+        scnd_b = 0
+    if len(tuple_b) == 0:
+        frst_b, scnd_b = 0, 0
+    if len(tuple_a) and len(tuple_b) == 2:
+        frst_a = tuple_a[0]
+        scnd_a = tuple_a[1]
+        frst_b = tuple_b[0]
+        scnd_b = tuple_b[1]
+    return (frst_a + frst_b, scnd_a + scnd_b)
