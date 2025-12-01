7fe5528b7713cf1a20c1e50a328331ccb048dbde
dfsd <dfs>
2025-12-01 08:12:35 +0000

    a1

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> operator <b>")
         sys.exit(1)
     else:
-        a, b = sys.argv[1], sys.argv[3]
+        a, b = int(sys.argv[1]), int(sys.argv[3])
         match sys.argv[2]:
             case "+":
                 print("{} + {} = {}".format(a, b, calc.add(a, b)))
@@ -22,3 +22,4 @@ if __name__ == "__main__":
                 print("Unknown operator. Available operators: +, -, * and /")
                 sys.exit(1)
         sys.exit(0)
+
