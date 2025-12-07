b92a1bc17edfdc82a680d7efe86711b99da2e460
user <email>
2025-12-07 05:25:09 +0000

    a1

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    new_dictionary = {}
+    list = []
     for i in a_dictionary.keys():
         if (a_dictionary[i] == value):
-            continue
-        new_dictionary[i] = a_dictionary[i]
-    return new_dictionary
+            list.append(i)
+    for i in list:        
+        del a_dictionary[i]
+    return a_dictionary
