dc7d1079b2420ffcc431f9af871d170500daf9dc
user <email>
2025-12-07 05:17:04 +0000

    a1

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    new_dictionary = {}
     for i in a_dictionary.keys():
         if (a_dictionary[i] == value):
-            del a_dictionary[i]
-    return a_dictionary
+            continue
+    new_dictionary[i] = a_dictionary[i]
+    return new_dictionary
