2cfd5ff3bcb0917a0914c7d5f58b99095445a5e8
user <email>
2025-12-07 05:50:18 +0000

    a1

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
 import sys
-   
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as e:
-        print("Exception: {}".format(str(e)))
-        return False
-    except Exception:
+    except Exception as e:
+        print("Exception: {}".format(str(e)), file=sys.stderr)
         return False
