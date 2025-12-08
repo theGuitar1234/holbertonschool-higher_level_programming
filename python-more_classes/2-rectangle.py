6313f0419fa9c35f0884e472ad8ea81ba32d095c
user <email>
2025-12-08 12:50:46 +0000

    a1

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+"""
+Docstring for 1-rectangle
+"""
+
+
+class Rectangle:
+
+    """
+    Docstring for Rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        if not isinstance(self.__width, int):
+            raise Exception("width must be an integer")
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        if not isinstance(self.__height, int):
+            raise Exception("height must be an integer")
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    def area(self):
+        return self.__height*self.__height
+
+    def perimeter(self):
+        return self.__width*2 + self.__height*2
