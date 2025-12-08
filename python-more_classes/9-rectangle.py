46dd10d2ae54bc9308260f440ee1cacf06d09b14
user <email>
2025-12-08 13:29:35 +0000

    a1

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -106,7 +106,7 @@ class Rectangle:
     @staticmethod
     def square(size):
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("width must be >= 0")
         return Rectangle(size, size)
