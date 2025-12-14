5b399417e1f82d266be642b09ab522936993d966
user <email>
2025-12-14 06:02:29 +0000

    a1

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,6 +5,6 @@
 def inherits_from(obj, a_class):
 
     """Checks if an object is an inherited instance of a class.i"""
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and obj.__class__ != a_class:
         return True
     return False
