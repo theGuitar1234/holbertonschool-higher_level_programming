e49cff8f2087309dd64562febc0e4adf2cdd481a
user <email>
2025-12-14 06:13:50 +0000

    a1

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -15,7 +15,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validate a parameter as an integer."""
-        if type(value) != int:
+        if value.__class__ != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
