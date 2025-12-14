2b015be72321ca5dfc4512e70a6cdc4896132aa7
user <email>
2025-12-14 05:37:20 +0000

    a1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,10 @@
 class MyList(list):
     """Implements sorted printing for the built-in list class."""
 
+    def __init__(self, *args):
+        if (len(args) != 0):
+            pass
+
     def print_sorted(self):
         """Print a list in sorted ascending order."""
         print(sorted(self))
