7c6c9f77e3cb6a4dcfea19379b0b1ceb5a0a72a1
user <email>
2025-12-14 06:41:25 +0000

    a1

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -3,13 +3,13 @@
 
 
 def print_stats(size, status_codes):
+ 
     """Print accumulated metrics."""
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
 
 if __name__ == "__main__":
-
 
     import sys
 
