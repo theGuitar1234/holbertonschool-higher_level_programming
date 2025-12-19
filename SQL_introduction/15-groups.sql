e16a7d4634ec58164294459630d509457adbee2e
user <email>
2025-12-19 09:32:26 +0000

    a1

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- agfsg
-SELECT score, COUNT(DISTINCT score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
