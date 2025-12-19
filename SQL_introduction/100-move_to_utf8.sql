791b4859fa80bebe2cc589db9e8eacb33bb61a31
user <email>
2025-12-19 09:50:39 +0000

    a1

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- agad
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE first_table CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(255) SET utf8mb4 COLLATE utf8_mb4_unicode_ci;
