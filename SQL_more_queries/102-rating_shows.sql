d74b8bf911fac34cf398d5867efda5a8cecfbad2
user <email>
2025-12-21 05:45:40 +0000

    a1

---
+++ SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- jhsgdjhfd
+SELECT tv_shows.title, IFNULL(SUM(tv_show_ratings.rate), 0) AS rating
+FROM tv_shows
+LEFT JOIN tv_show_ratings
+    ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.id
+ORDER BY rating DESC;
