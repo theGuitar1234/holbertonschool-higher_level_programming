11dfbc85b60b1340291027d4f76593977ec8251c
user <email>
2025-12-21 05:42:52 +0000

    a1

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -1,8 +1,8 @@
 -- kasdhjfkjd
-SELECT title FROM tv_shows
-WHERE id NOT IN (
-    SELECT id FROM tv_genres g1
-    JOIN tv_genres g2 ON g1.genre_id = g2.id
+SELECT tv_shows.title FROM tv_shows
+WHERE tv_shows.id NOT IN (
+    SELECT g1.genre_id FROM tv_genres g1
+    JOIN tv_genres g2 ON g1.genre_id = g2.genre_id
     WHERE g1.name = 'Comedy'
 )
-ORDER BY title;
+ORDER BY tv_shows.title;
