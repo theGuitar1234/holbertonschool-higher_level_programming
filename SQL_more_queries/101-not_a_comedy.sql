48203943eb6046d0be8826f29466186930465fea
user <email>
2025-12-21 05:44:32 +0000

    a1

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -1,8 +1,11 @@
--- kasdhjfkjd
-SELECT tv_shows.title FROM tv_shows
+-- kjfghkdfg
+SELECT tv_shows.title
+FROM tv_shows
 WHERE tv_shows.id NOT IN (
-    SELECT g1.genre_id FROM tv_genres g1
-    JOIN tv_genres g2 ON g1.genre_id = g2.genre_id
-    WHERE g1.name = 'Comedy'
+    SELECT tv_show_genres.show_id
+    FROM tv_show_genres
+    JOIN tv_genres
+        ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
 )
 ORDER BY tv_shows.title;
